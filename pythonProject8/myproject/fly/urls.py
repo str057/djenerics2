49d2debfdd52de8d4df3b0064f69c4dde8348f49
str057 <error.blin@gmail.com>
2025-10-01 16:51:13 +0300

    Delete pythonProject8 directory

--- pythonProject8/myproject/fly/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "fly"
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:pk>/", views.prod_detail, name="prod_detail"),
-]
